4541d5bbb34b818ec5af598136877f3c4b73a303
David Heidrich <me@bowlingx.com>
2013-07-18 17:37:37 +0200

    fixed test

--- src/test/scala/com/bowlingx/websocket/WebsocketScalatraServletSpec.scala
+++ src/test/scala/com/bowlingx/websocket/WebsocketScalatraServletSpec.scala
@@ -1,16 +1,16 @@
-package com.bowlingx.websocket-scalatra
+package com.bowlingx.websocket
 
 import org.scalatra.test.specs2._
 
 // For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html
 class WebsocketScalatraServletSpec extends ScalatraSpec { def is =
   "GET / on MainServlet"                     ^
-    "should return status 200"                  ! root200^
+    "should return status 200"                   ! root200^
                                                 end
 
   addServlet(classOf[MainServlet], "/*")
 
-  def root200 = get("/") {
+  def root200 = get("/hello") {
     status must_== 200
   }
 }
